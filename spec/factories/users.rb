8bfabc3eef42b7c487831be0d4d65dc74e76fd01
lucaleivaloop <32399007+lucaleivaloop@users.noreply.github.com>
2020-02-27 13:51:09 -0300

    Added circle ci config plus modify github files (#25)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -16,5 +16,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    password { Faker::Internet.password }
+    country
   end
 end

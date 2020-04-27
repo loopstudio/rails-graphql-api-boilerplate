a0ef3c85b91e142a4c6d9820e4a2709d2afc673d
Mauricio Coniglio <mconiglio.782@gmail.com>
2020-04-27 14:49:24 -0300

    RGQL-14 Remove events model and endpoint
    * Also remove the query analyzer considering that its main functionality
      was to register events

--- app/graphql/rails_api_boilerplate_schema.rb
+++ app/graphql/rails_api_boilerplate_schema.rb
@@ -2,7 +2,6 @@ class RailsApiBoilerplateSchema < GraphQL::Schema
   use GraphQL::Subscriptions::ActionCableSubscriptions, redis: Redis.new
   use GraphQL::Batch
 
-  query_analyzer(QueryAnalyzer.new)
   mutation(Types::MutationType)
   query(Types::QueryType)
   subscription(Types::SubscriptionType)
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,6 +1,5 @@
 module Types
   class QueryType < Types::BaseObject
     include Types::Queries::UserQueries
-    include Types::Queries::EventQueries
   end
 end

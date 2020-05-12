e720e2e0de8444e8b90fc430d4fe30ac8428e060
Agustin Perez <aperezcristina@gmail.com>
2020-05-11 22:26:23 -0300

    really

--- runLinters.sh
+++ runLinters.sh
@@ -4,4 +4,4 @@ git fetch origin master:refs/remotes/origin/master
 files_diff=$(git diff --diff-filter=ACMRTUXB --name-only origin/master... | xargs | sed "s/\\n/\\s/")
 
 bundle exec rubocop --force-exclusion $files_diff
-bundle exec reek --force-exclusion -c .reek.yml $files_diff
+bundle exec reek -c .reek.yml --force-exclusion $files_diff

bce547e0799ed9fb421a58064b5b0cbf120859d4
diebas <40240968+diebas@users.noreply.github.com>
2020-05-27 15:46:09 -0300

    Pagination instructions and setup (#95)

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,4 +1,15 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
+    def self.connection_type
+      connection_class = super
+
+      connection_class.define_method(:total_count) do
+        object.nodes.size
+      end
+
+      connection_class.send(:field, :total_count, Integer, null: false)
+
+      connection_class
+    end
   end
 end

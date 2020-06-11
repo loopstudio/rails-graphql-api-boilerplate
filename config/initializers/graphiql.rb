fe645bf78d10c251e488a3f9630601f1a8ff78a2
lucaleivaloop <lleiva@loopstud.io>
2020-06-11 14:36:49 -0300

    Add handling for when the var is not set

--- config/initializers/graphiql.rb
+++ config/initializers/graphiql.rb
@@ -1,5 +1,4 @@
-if Rails.env.development?
-  GraphiQL::Rails.config.headers['Authorization'] = lambda { |_context|
-    "bearer #{ENV['GRAPHIQL_SESSION_TOKEN']}"
-  }
-end
+GraphiQL::Rails.config.headers['Authorization'] = lambda { |_context|
+  token = ENV['GRAPHIQL_SESSION_TOKEN']
+  token ? "bearer #{token}" : ''
+}

676ee60cf4ed41bba5cdab077c154000da51203d
Mauricio Coniglio <mconiglio.782@gmail.com>
2021-06-04 19:38:14 -0300

    Fix exception handlers which were always rescuing from standard error

--- app/graphql/rails_api_boilerplate_schema.rb
+++ app/graphql/rails_api_boilerplate_schema.rb
@@ -10,16 +10,16 @@ class RailsApiBoilerplateSchema < GraphQL::Schema
   query(Types::QueryType)
   subscription(Types::SubscriptionType)
 
-  rescue_from(StandardError) do |message|
-    GraphQL::ExecutionError.new(message, extensions: { code: :internal_server_error })
-  end
-
-  rescue_from(ActiveRecord::RecordInvalid) do |_message|
+  rescue_from(ActiveRecord::RecordInvalid) do |exception|
     errors = exception.record.errors.messages.to_json
     GraphQL::ExecutionError.new(errors, extensions: { code: :bad_request })
   end
 
-  rescue_from(ActiveRecord::RecordNotFound) do |message|
-    GraphQL::ExecutionError.new(message, extensions: { code: :not_found })
+  rescue_from(ActiveRecord::RecordNotFound) do |exception|
+    GraphQL::ExecutionError.new(exception, extensions: { code: :not_found })
+  end
+
+  rescue_from(StandardError) do |exception|
+    GraphQL::ExecutionError.new(exception, extensions: { code: :internal_server_error })
   end
 end
